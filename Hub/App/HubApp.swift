ecb1ea9b22e0fd9aec03b7b126ee99c093dd61dd
Dmitry <d@kozlov.ru>
2025-07-05 13:14:29 +0300

    App now closes when you close last window

--- Hub/App/HubApp.swift
+++ Hub/App/HubApp.swift
@@ -9,9 +9,23 @@ import SwiftUI
 
 @main
 struct HubApp: App {
+#if os(macOS)
+  @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+#endif
   var body: some Scene {
     WindowGroup {
       ContentView()
     }
   }
 }
+
+#if os(macOS)
+import Foundation
+import AppKit
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+  func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+    return true
+  }
+}
+#endif
