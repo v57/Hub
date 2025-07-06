3a33dd3ca7d7066d6fb172f3f55e38a069d94fc5
Dmitry <d@kozlov.ru>
2025-07-06 13:23:27 +0300

    Icon view: Added dark mode style

--- Hub/Pages/Launcher/IconView.swift
+++ Hub/Pages/Launcher/IconView.swift
@@ -7,16 +7,15 @@
 
 import SwiftUI
 
-struct Icon: Codable {
+struct Icon: Codable, Hashable {
   var symbol: SFSymbolIcon?
   var text: TextIcon?
-  struct SFSymbolIcon: Codable {
+  struct SFSymbolIcon: Codable, Hashable {
     var name: String
-    var foreground: String?
-    var background: String?
-    func body(cornerRadius: CGFloat) -> some View {
+    var colors: IconColors?
+    func body(cornerRadius: CGFloat, dark: Bool) -> some View {
       GeometryReader { view in
-        if let color = background?.color {
+        if let color = colors?.background(dark: dark)?.color {
           RoundedRectangle(cornerRadius: cornerRadius).fill(RadialGradient(colors: [color.opacity(0.6), color], center: .topTrailing, startRadius: 0, endRadius: view.size.height))
         } else {
           RoundedRectangle(cornerRadius: cornerRadius).fill(Color.gray.opacity(0.2))
@@ -27,42 +26,56 @@ struct Icon: Codable {
       }
     }
   }
-  struct TextIcon: Codable {
+  struct TextIcon: Codable, Hashable {
     var name: String
-    var foreground: String?
-    var background: String?
-    private var foregroundGradient: AnyGradient {
-      foreground?.color?.gradient ?? Color.primary.gradient
-    }
-    private var backgroundGradient: AnyGradient {
-      background?.color?.gradient ?? Color.gray.opacity(0.2).gradient
-    }
-    func body(cornerRadius: CGFloat) -> some View {
+    var colors: IconColors?
+    func body(cornerRadius: CGFloat, dark: Bool) -> some View {
       GeometryReader { view in
-        if let color = background?.color {
+        if let color = colors?.background(dark: dark)?.color {
           RoundedRectangle(cornerRadius: cornerRadius).fill(RadialGradient(colors: [color.opacity(0.6), color], center: .topTrailing, startRadius: 0, endRadius: view.size.height))
         } else {
           RoundedRectangle(cornerRadius: cornerRadius).fill(Color.gray.opacity(0.2))
         }
         Text(name)
           .font(.system(size: view.size.height * 0.5, weight: .bold, design: .rounded))
-          .foregroundStyle(foregroundGradient)
+          .foregroundStyle(colors?.foreground(dark: dark)?.color ?? .primary)
           .minimumScaleFactor(0.01)
           .padding(.horizontal, 4)
           .frame(maxWidth: .infinity, maxHeight: .infinity)
       }
     }
   }
-  func body(cornerRadius: CGFloat) -> some View {
+  func body(cornerRadius: CGFloat, dark: Bool) -> some View {
     ZStack {
       if let symbol {
-        symbol.body(cornerRadius: cornerRadius)
+        symbol.body(cornerRadius: cornerRadius, dark: dark)
       } else if let text {
-        text.body(cornerRadius: cornerRadius)
+        text.body(cornerRadius: cornerRadius, dark: dark)
       }
     }.aspectRatio(1, contentMode: .fit)
   }
 }
+struct IconColors: Codable, Hashable {
+  var foreground: String?
+  var foregroundDark: String?
+  var background: String?
+  var backgroundDark: String?
+  fileprivate func foreground(dark: Bool) -> String? {
+    dark ? foregroundDark ?? foreground : foreground
+  }
+  fileprivate func background(dark: Bool) -> String? {
+    dark ? backgroundDark ?? background : background
+  }
+}
+struct IconView: View {
+  let icon: Icon
+  var cornerRadius: CGFloat = 0
+  @Environment(\.colorScheme) var colorScheme
+  var body: some View {
+    icon.body(cornerRadius: cornerRadius, dark: colorScheme == .dark)
+  }
+}
+
 
 extension String {
   var color: Color? {
@@ -82,10 +95,10 @@ extension Color {
 
 #Preview {
   HStack {
-    Icon(symbol: .init(name: "apple.intelligence")).body(cornerRadius: 8)
-    Icon(text: .init(name: "R", foreground: "ffffff", background: "ff0000")).body(cornerRadius: 8)
-    Icon(text: .init(name: "G", foreground: "00ff00", background: "000000")).body(cornerRadius: 8)
-    Icon(text: .init(name: "B", foreground: "0000ff")).body(cornerRadius: 8)
-    Icon(text: .init(name: "W", foreground: "ffffff")).body(cornerRadius: 8)
+    IconView(icon: Icon(symbol: .init(name: "apple.intelligence")))
+    IconView(icon: Icon(text: .init(name: "R", colors: IconColors(foreground: "ffffff", background: "ff0000"))))
+    IconView(icon: Icon(text: .init(name: "G", colors: IconColors(foreground: "00ff00", background: "000000"))))
+    IconView(icon: Icon(text: .init(name: "B", colors: IconColors(foreground: "0000dd", foregroundDark: "aaaaff"))))
+    IconView(icon: Icon(text: .init(name: "W", colors: IconColors(foreground: "ffffff"))))
   }.frame(height: 44).padding()
 }
