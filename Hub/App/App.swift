32f036a41ea333af2f58a00d0dfce39da797d1ff
Dmitry <d@kozlov.ru>
2025-07-20 16:36:54 +0300

    Added iOS support

--- Hub/App/App.swift
+++ Hub/App/App.swift
@@ -16,67 +16,64 @@ struct ContentView: View {
     case storage
     case app(AppHeader)
   }
-  @State var sideView: SideView = .storage
+  @State var sideView: SideView? = .storage
   @State var statusBadges = StatusBadges()
   let hubs = Hubs.main
   var body: some View {
     NavigationSplitView {
-#if os(macOS)
-      List(selection: $sideView) { listContent }
-#else
-      List { listContent }
-      #endif
+      List { listContent }.navigationDestination(for: SideView.self) { value in
+        destination(sideView: value)
+      }
     } detail: {
-      NavigationStack {
-        switch sideView {
-        case .services:
-          if let hub = hubs.selectedHub {
-            Services().environment(hub)
-          }
-        case .cluster:
-          ConnectionsView()
-        case .launcher:
-          if let hub = hubs.selectedHub {
-            LauncherView().environment(hub)
-          }
-        case .security:
-          if let hub = hubs.selectedHub {
-            SecurityView().environment(hub)
-          }
-        case .app(let header):
-          if let hub = hubs.selectedHub {
-            ServiceView(header: header).environment(hub)
-          }
-        case .storage:
-          if let hub = hubs.selectedHub {
-            StorageView().environment(hub)
-          }
+      
+    }
+  }
+  func destination(sideView: SideView) -> some View {
+    NavigationStack {
+      switch sideView {
+      case .services:
+        if let hub = hubs.selectedHub {
+          Services().environment(hub)
+        }
+      case .cluster:
+        ConnectionsView()
+      case .launcher:
+        if let hub = hubs.selectedHub {
+          LauncherView().environment(hub)
+        }
+      case .security:
+        if let hub = hubs.selectedHub {
+          SecurityView().environment(hub)
+        }
+      case .app(let header):
+        if let hub = hubs.selectedHub {
+          ServiceView(header: header).environment(hub)
+        }
+      case .storage:
+        if let hub = hubs.selectedHub {
+          StorageView().environment(hub)
         }
       }
     }
   }
   @ViewBuilder
   var listContent: some View {
-    Text("Connections")
-      .id(SideView.cluster)
+    NavigationLink("Connections", value: SideView.cluster)
     if let hub = hubs.selectedHub {
       Section(hub.settings.name) {
-        Text("Services").badge(statusBadges.services)
-          .id(SideView.services)
-        Text("Launcher")
-          .id(SideView.launcher)
-        Text("Security").badge(statusBadges.security ?? 0)
-          .badgeProminence(.increased)
-          .id(SideView.security)
-        Text("Storage")
-          .id(SideView.storage)
+        NavigationLink("Services", value: SideView.services)
+          .badge(statusBadges.services)
+        NavigationLink("Launcher", value: SideView.launcher)
+        NavigationLink("Security", value: SideView.security)
+          .badge(statusBadges.security ?? 0).badgeProminence(.increased)
+        NavigationLink("Storage", value: SideView.storage)
       }.hubStream("hub/status/badges", initial: StatusBadges(), to: $statusBadges)
         .environment(hub)
       
       if let apps = statusBadges.apps, !apps.isEmpty {
         Section("Apps") {
           ForEach(apps) { app in
-            Text(app.name).id(SideView.app(app))
+            NavigationLink(app.name, value: SideView.app(app))
               .foregroundStyle(app.isOnline ? .primary : .tertiary)
           }
         }
