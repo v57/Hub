34bd65808abbf3375619c80031e0d1670c024783
Dmitry <d@kozlov.ru>
2025-10-05 17:07:56 +0700

    Translate view: Added realtime translation

--- Hub/Services/Translate/Translate view.swift
+++ Hub/Services/Translate/Translate view.swift
@@ -8,41 +8,60 @@
 import SwiftUI
 import Translation
 
+@Observable
+class Translator {
+  var text: String = ""
+  var result: String = ""
+  var source: String = "en"
+  var target: String = "de"
+}
+
 @available(macOS 15.0, *)
 struct TranslateView: View {
-  @State var text: String = "Hello"
-  @State var translation = ""
-  @State var source: String = "en"
-  @State var target: String = "de"
+  @Bindable var translator = Translator()
+  @State var configuration: TranslationSession.Configuration?
   @State var availability = LanguageAvailability()
   @State var languages = [String]()
-  @State var configuration: TranslationSession.Configuration?
+  @State var session: TranslationSession?
+  @State var currentTask: Task<Void, Error>?
+  var taskId: String {
+    guard let session else { return "" }
+    return (session.sourceLanguage?.minimalIdentifier ?? "") + (session.targetLanguage?.minimalIdentifier ?? "")
+  }
   var body: some View {
     VStack {
       HStack {
-        Picker("Source", selection: $source) {
+        Picker("Source", selection: $translator.source) {
           ForEach(languages, id: \.self) { language in
             Text(language.languageName).tag(language)
           }
         }
-        Picker("Target", selection: $target) {
+        Picker("Target", selection: $translator.target) {
           ForEach(languages, id: \.self) { language in
             Text(language.languageName).tag(language)
           }
         }
       }
-      TextField("Text to translate", text: $text)
-      Text(translation).translationTask(configuration) { session in
-        Task {
-          let result = try await session.translate(text)
-          self.translation = result.targetText
+      TextField("Text to translate", text: $translator.text)
+      Text(translator.result).contentTransition(.numericText()).translationTask(configuration) { session in
+        currentTask?.cancel()
+        currentTask = Task {
+          if #available(macOS 26.0, *) {
+            for await text in Observations({ translator.text }) {
+              guard !text.isEmpty else { continue }
+              let result = try await session.translate(text).targetText
+              withAnimation {
+                translator.result = result
+              }
+            }
+          }
         }
-      }.id(text)
+      }
     }.task {
       languages = await availability.supportedLanguages.map(\.minimalIdentifier).sorted(by: { $0.languageName < $1.languageName })
-    }.task(id: source + target) {
-      configuration = TranslationSession.Configuration(source: .init(identifier: source), target: .init(identifier: target))
-    }
+    }.task(id: translator.source + translator.target) {
+      configuration = TranslationSession.Configuration(source: .init(identifier: translator.source), target: .init(identifier: translator.target))
+    }.padding()
   }
 }
 extension String {
