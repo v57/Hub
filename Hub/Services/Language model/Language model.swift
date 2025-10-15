6cda84b330ea106928ff1002ece99fde0af2cf0c
Dmitry <d@kozlov.ru>
2025-10-16 00:19:36 +0700

    Added language model

---
+++ Hub/Services/Language model/Language model.swift
@@ -0,0 +1,74 @@
+//
+//  Foundation models.swift
+//  Hub
+//
+//  Created by Linux on 08.10.25.
+//
+
+import SwiftUI
+import FoundationModels
+
+@available(macOS 26.0, *)
+struct ModelChat: View {
+  @State var session = LanguageModelSession()
+  @State var messages: [Message] = []
+  @State var text: String = ""
+  var body: some View {
+    List(messages) { message in
+      Text(message.text).contentTransition(.numericText())
+    }
+    HStack {
+      TextField("Type your message...", text: $text)
+      Button("Send") {
+        Task { try await send(text: text) }
+      }.disabled(session.isResponding)
+    }.padding()
+  }
+  @Observable
+  class Message: Identifiable {
+    var text: AttributedString
+    init(text: String) {
+      self.text = text.markdown
+    }
+  }
+  func send(text: String) async throws {
+    self.text = ""
+    messages.append(Message(text: text))
+    Task {
+      let message = Message(text: "responding...")
+      messages.append(message)
+      for try await response in session.streamResponse(to: text) {
+        withAnimation {
+          message.text = response.content.markdown
+        }
+      }
+    }
+  }
+}
+extension String {
+  var markdown: AttributedString {
+    let options = AttributedString.MarkdownParsingOptions(interpretedSyntax: .inlineOnly, failurePolicy: .returnPartiallyParsedIfPossible)
+    return (try? AttributedString(markdown: self, options: options)) ?? AttributedString(self)
+  }
+}
+
+@available(macOS 26.0, *)
+#Preview {
+  ModelChat()
+}
+
+
+
+
+
+
+
+enum ALM {
+  struct Message: Codable {
+    let role: String
+    let content: String
+  }
+  struct ChatRequest: Codable {
+    let messages: [Message]
+  }
+}
