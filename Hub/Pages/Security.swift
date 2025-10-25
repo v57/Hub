a3e89e107e3da0ec4c6a22dd134e662569eddc06
Dmitry <d@kozlov.ru>
2025-10-25 19:06:06 +0700

    Security: Added add owner button

--- Hub/Pages/Security.swift
+++ Hub/Pages/Security.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct SecurityView: View {
   @Environment(Hub.self) var hub
   @State var pending: [PendingAuthorization] = []
+  @State var ownerKey: String = ""
+  @State var addingOwner: Bool = false
   var body: some View {
     List(pending) { item in
       HStack {
@@ -24,11 +26,35 @@ struct SecurityView: View {
           try await hub.client.send("hub/permissions/add", Allow(services: item.pending, permission: item.id))
         }
       }
+    }.safeAreaInset(edge: .top) {
+      HStack {
+        if hub.permissions.contains("owner") {
+          if addingOwner {
+            SecureField("Key", text: $ownerKey)
+            AsyncButton("Add") {
+              addingOwner = false
+              do {
+                try await hub.client.send("auth/permissions/add", AddPermissions(key: ownerKey, permissions: ["owner"]))
+              } catch {
+                print(error)
+              }
+            }.disabled(ownerKey.isEmpty)
+          } else {
+            Button("Add owner") {
+              addingOwner = true
+            }
+          }
+        }
+      }.frame(maxWidth: .infinity, alignment: .trailing).padding(.horizontal)
     }.navigationTitle("Security").hubStream("hub/permissions/pending", initial: [], to: $pending)
   }
   struct Allow: Encodable {
     let services: [String]
     let permission: String
+  }
+  struct AddPermissions: Encodable {
+    let key: String
+    let permissions: [String]
   }
   struct PendingAuthorization: Identifiable, Decodable {
     let id: String
